2a8e9322dff18dacb53db112029db1a0bac9978a
Isaac Burgos <isaacburgos543@gmail.com>
2026-02-26 22:41:37 -0500

    feature:agregando estructura incial del parcial

---
+++ DOSW-ParcialT1/src/main/java/edu/dosw/parcial/DoswParcialT1Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswParcialT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswParcialT1Application.class, args);
+	}
+
+}
